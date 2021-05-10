18d7a8a8237f5895992af1a6c1498454aa380340
prospero-romain <contact@chevalier-prospero.fr>
2021-05-10 15:59:52 +0200

    builder/controller/moddels

---
+++ src/builders/exemple.builders.js
@@ -0,0 +1,10 @@
+let mongoose = require('mongoose');
+let Test = mongoose.model('Test');
+
+exports.getTests = function(){
+   return Test.find({})
+       .populate({
+           path:'actions',
+           select: 'nom'
+       });
+};
